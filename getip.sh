141ae9a29611d305dda902c51b620d36574a95c4
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-06-27 10:25:42 -0300

    Update getip.sh

--- getip.sh
+++ getip.sh
@@ -136,7 +136,6 @@ echo ""
 else
 
 printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
-printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
 arch=$(uname -a | grep -o 'arm' | head -n1)
 arch2=$(uname -a | grep -o 'Android' | head -n1)
 if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
